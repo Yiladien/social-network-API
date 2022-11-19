fe7d355da4d56b96a255e562cffe71ed89a610ff
yiladien@hotmail.com <yiladien@hotmail.com>
2022-11-19 00:50:04 -0600

    added reaction schema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -2,6 +2,38 @@ import dateFormat, { masks } from "dateformat";
 
 const { Schema, model } = require("mongoose");
 
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const ThoughtsSchema = new Schema(
   {
     thoughtText: {
